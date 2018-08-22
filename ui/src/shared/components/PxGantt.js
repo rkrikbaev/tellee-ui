12eed2008c25d1c4f5ba300e8236e9bd17b837b9
galym <almabek.galym@gmail.com>
2018-08-22 13:53:37 +0600

    set reload graph when new data comes

--- ui/src/shared/components/PxGantt.js
+++ ui/src/shared/components/PxGantt.js
@@ -108,6 +108,7 @@ class PxGantt extends Component {
     if (!this.isJsonString(prefix)) {
       return 'white'
     }
+    console.log(state)
     prefix = JSON.parse(prefix)
     switch (state) {
       case 4:
